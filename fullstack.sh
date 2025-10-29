feed6d8e730893545a7b1f2d43e5846b1a07261c
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2025-10-29 20:19:36 +0100

    Correct typo

--- fullstack.sh
+++ fullstack.sh
@@ -398,4 +398,4 @@ git add -A
 git commit -m "Project skeleton created from script" >/dev/null
 
 echo -e "${G}Success!${N}"
-echo -e "Open ${LINK}$PWD\a$PWD${LINK}\a in your editor and see REAME.md"
+echo -e "Open ${LINK}$PWD\a$PWD${LINK}\a in your editor and see README.md"
